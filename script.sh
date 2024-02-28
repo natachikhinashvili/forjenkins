61d3cc7c8f190f244ddba4c8ce4ce54fa2325ddd
nata tchikhinashvili <86610861+natachikhinashvili@users.noreply.github.com>
2024-02-28 21:17:21 +0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Find the container ID(s) running on port 5000
-CONTAINER_IDS=$(docker ps -q --filter "publish=5000/tcp")
+CONTAINER_IDS=$(/usr/local/bin/docker ps -q --filter "publish=5000/tcp")
 
 # Check if any container is running on port 5000
 if [ -z "$CONTAINER_IDS" ]; then
@@ -9,7 +9,7 @@ if [ -z "$CONTAINER_IDS" ]; then
 else
     # Loop through each container ID to stop and remove it
     for CONTAINER_ID in $CONTAINER_IDS; do
-        docker system prune -a >/dev/null
+        /usr/local/bin/docker system prune -a >/dev/null
         echo "Container $CONTAINER_ID running on port 5000 has been stopped and removed."
     done
 fi
