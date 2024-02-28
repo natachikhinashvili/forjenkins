71d90db10a4e8d6a22957f47853fdf1d8b64aa17
nata tchikhinashvili <86610861+natachikhinashvili@users.noreply.github.com>
2024-02-28 19:19:44 +0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ if [ -z "$CONTAINER_IDS" ]; then
 else
     # Loop through each container ID to stop and remove it
     for CONTAINER_ID in $CONTAINER_IDS; do
-        docker stop "$CONTAINER_ID" >/dev/null && docker rm "$CONTAINER_ID" >/dev/null
+        docker system prune -a >/dev/null
         echo "Container $CONTAINER_ID running on port 5000 has been stopped and removed."
     done
 fi
